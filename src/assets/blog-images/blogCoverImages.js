4ae23baab90116c15d319d374235b64ade049d4b
Grace Manning <gracebmanning@gmail.com>
2023-12-07 21:04:35 -0800

    remove Blog files

--- src/assets/blog-images/blogCoverImages.js
+++
@@ -1,6 +0,0 @@
-import graduated from './graduated/diploma1 - cropped.jpg';
-import linefontCover from './linefont/linefont-cover.png';
-
-export{
-    graduated, linefontCover
-};
--- src/assets/blog-images/graduated/index.js
+++
@@ -1,6 +0,0 @@
-import diploma1 from './diploma1.jpg';
-import diploma2 from './diploma2.jpg';
-
-export{
-    diploma1, diploma2
-};
